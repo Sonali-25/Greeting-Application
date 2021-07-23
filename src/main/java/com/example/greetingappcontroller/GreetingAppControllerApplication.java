2c9d37726bc614639a69c6b9b90546b4cf0a85eb
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-07-23 11:08:05 +0530

    Greeting App Controller with different Http

---
+++ src/main/java/com/example/greetingappcontroller/GreetingAppControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.greetingappcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingAppControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/greetingappcontroller/GreetingAppControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greetingappcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
